03fc106b13e5d8b56ff140a9dac153d653d8470c
Farya <faryabakhsh@gmail.com>
2022-06-29 23:14:29 +0500

    algo

---
+++ dijkstra.js
@@ -0,0 +1,67 @@
+class PriorityQueue {
+    constructor(){
+      this.values = [];
+    }
+    enqueue(val, priority) {
+      this.values.push({val, priority});
+      this.sort();
+    };
+    dequeue() {
+      return this.values.shift();
+    };
+    sort() {
+      this.values.sort((a, b) => a.priority - b.priority);
+    };
+  }
+
+  class WeightedGraph {
+    constructor() {
+        this.adjacencyList = {};
+    }
+    addVertex(vertex){
+        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+    }
+    addEdge(vertex1,vertex2, weight){
+        this.adjacencyList[vertex1].push({node:vertex2,weight});
+        this.adjacencyList[vertex2].push({node:vertex1, weight});
+    }
+    Dijkstra(start, finish){                        // the function hsould accept a starting and an ending vertex
+         const nodes = new PriorityQueue();         //making a new priority queue
+        const distances = {};
+        const previous = {};
+
+        //build up initial state ----- looping over the entire adjacency list
+        for(let vertex in this.adjacencyList) {
+            if(vertex === start) {
+                distances[vertex] = 0;                              //create an objecy (distances) and set each key to be every vertex in the adjacency list with the value of infinity, except for the starting vertex ehich should  have a value of 0
+                nodes.enqueue(vertex, 0);
+            } else {
+                distances[vertex] = Infinity;
+                nodes.enqueue(vertex, Infinity);
+            }
+            previous[vertex] = null;                        //creating another object called previousand set each key to be every vertes in the adjacency list with a value of null
+        }
+        
+    }
+}
+
+
+var graph = new WeightedGraph()
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addVertex("D");
+graph.addVertex("E");
+graph.addVertex("F");
+
+graph.addEdge("A","B", 4);
+graph.addEdge("A","C", 2);
+graph.addEdge("B","E", 3);
+graph.addEdge("C","D", 2);
+graph.addEdge("C","F", 4);
+graph.addEdge("D","E", 3);
+graph.addEdge("D","F", 1);
+graph.addEdge("E","F", 1);
+
+
+graph.Dijkstra("A", "E");
---
+++ graphtraversal.js
@@ -0,0 +1,25 @@
+// visiting, updating, checking each vertex in a graph
+
+// USES:
+// peer to peer Networking
+// web crawlers == wikipedia
+// finding closeset matching recommendation ----- WebGLTransformFeedback, instagram friend recommendations
+// shortest path problems
+
+// DFS:
+// continuing to flow the neighbours 
+
+// -recursively
+// -iteratively
+
+// DFS PSEUDOCODE:
+
+// DFS(vertex):
+//     if vertex is empty
+//         return (this is base case)
+//     add vertex to result list
+//     mark vertex as visited
+//     for each neighbor in vertex neighbors:
+//         if neighbor is not visited:
+//             recursively call DFS    on neighbor
+
