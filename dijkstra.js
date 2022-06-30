afb599962642846c32112ad45eea70479788661d
Farya <faryabakhsh@gmail.com>
2022-06-30 19:36:16 +0500

    updated algorithm with real values

--- dijkstra.js
+++ dijkstra.js
@@ -14,7 +14,7 @@ class PriorityQueue {
     };
   }
 
-  class WeightedGraph {
+class WeightedGraph {
     constructor() {
         this.adjacencyList = {};
     }
@@ -29,6 +29,8 @@ class PriorityQueue {
          const nodes = new PriorityQueue();         //making a new priority queue
         const distances = {};
         const previous = {};
+        let smallest;
+        let path = [] //to return at end
 
         //build up initial state ----- looping over the entire adjacency list
         for(let vertex in this.adjacencyList) {
@@ -41,27 +43,55 @@ class PriorityQueue {
             }
             previous[vertex] = null;                        //creating another object called previousand set each key to be every vertes in the adjacency list with a value of null
         }
-        
-    }
+        // as long as there is something to visit
+        while(nodes.values.length) {
+          smallest =  nodes.dequeue().val;                //dequeue a vertex from a priority queue
+          if(smallest === finish)    {                 // if the vertex is the same as the ending vertex  -- we are done!
+        //WE SFRE DONE
+                //SeattleUILD UP PSFTH TO RETURN SFT END
+                while(previous[smallest]){
+                  path.push(smallest);
+                  smallest = previous[smallest];
+              }
+              break;
+          } 
+          if(smallest || distances[smallest] !== Infinity){
+              for(let neighbor in this.adjacencyList[smallest]){
+                  //find neighboring node
+                  let nextNode = this.adjacencyList[smallest][neighbor];
+                  //calculate new distance to neighboring node
+                  let candidate = distances[smallest] + nextNode.weight;
+                  let nextNeighbor = nextNode.node;
+                  if(candidate < distances[nextNeighbor]){
+                      //updating new smallest distance to neighbor
+                      distances[nextNeighbor] = candidate;
+                      //updating previous - How we got to neighbor
+                      previous[nextNeighbor] = smallest;
+                      //enqueue in priority queue with new priority
+                      nodes.enqueue(nextNeighbor, candidate);
+                  }
+              }
+          }
+      }
+      return path.concat(smallest).reverse();     
+  }
 }
 
-
 var graph = new WeightedGraph()
-graph.addVertex("A");
-graph.addVertex("B");
-graph.addVertex("C");
-graph.addVertex("D");
-graph.addVertex("E");
-graph.addVertex("F");
+graph.addVertex("SF");
+graph.addVertex("Seattle");
+graph.addVertex("Idaho");
+graph.addVertex("Chicago");
+graph.addVertex("NYC");
 
-graph.addEdge("A","B", 4);
-graph.addEdge("A","C", 2);
-graph.addEdge("B","E", 3);
-graph.addEdge("C","D", 2);
-graph.addEdge("C","F", 4);
-graph.addEdge("D","E", 3);
-graph.addEdge("D","F", 1);
-graph.addEdge("E","F", 1);
+graph.addEdge("SF","Seattle", 3);
+graph.addEdge("SF","Idaho", 5);
+graph.addEdge("Seattle","Idaho", 1);
+graph.addEdge("Seattle","Chicago", 2);
+graph.addEdge("Idaho","Chicago", 3);
+graph.addEdge("Chicago","NYC", 4);
+graph.addEdge("Idaho","NYC", 6);
 
 
-graph.Dijkstra("A", "E");
+
+console.log(graph.Dijkstra("SF", "NYC"));
